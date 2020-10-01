8d847989ba6b8684954ceca5560c8a88c56282d1
km <35552602+catnap7777@users.noreply.github.com>
2020-10-01 10:05:49 -0500

    TableViewAppWithAlert Done

--- MAD157Module7TableViewExercisesKarenM/ViewController.swift
+++ MAD157Module7TableViewExercisesKarenM/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 //******************************************************************
-//***..  Part 2 - TableViewVisualApp
+//***..  Part 3 - TableViewAppWithAlert
 //******************************************************************
 
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
    
+    @IBOutlet var petTable: UITableView!
+    
     let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+    
+    let cellID = "cellID"
    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
+        petTable.delegate = self
+        petTable.dataSource = self
+        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,10 +35,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
-        cell?.textLabel?.text = petArray[indexPath.row]
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if (cell == nil ) {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+       }
+        
+       cell?.textLabel?.text = petArray[indexPath.row]
         return cell!
     }
     
+    func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedItem = petArray[indexPath.row]
+        
+        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            action -> Void in
+            //Just dismiss the action sheet
+        })
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true , completion: nil )
+    }
+
 }
 
